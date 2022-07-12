21fc62ca0a6fc7fd9231cacdd3522ec9de6f9985
amber1o <100294813+amber1o@users.noreply.github.com>
2022-07-11 23:17:25 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -33,7 +33,7 @@ class Solution:
         s = s.lower().replace(' ', '')
         if len(s) >= 6:
             if s == s[::-1]:
-                if s.isalnum():
+                if s.isalpha():
                     return True
                 else: 
                     return False
